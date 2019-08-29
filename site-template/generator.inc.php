0b1f00b50261a7f3144851176ae0821cf728cdce
Dejan Boras <de.jean.7777@protonmail.com>
2019-08-29 17:25:09 +0200

    Update template site

--- site-template/generator.inc.php
+++ site-template/generator.inc.php
@@ -4,9 +4,3 @@ Common::$copy_list = ['css', 'js'];
 
 Common::Add('__HEADER__', 'header.html');
 Common::Add('__FOOTER__', 'footer.html');
-
-Pages::add_post('Start', 'start.html', '2019-05-28');
-Pages::add_post('Another one', 'start.html', '2019-05-29');
-Pages::add_post('Where to', 'start.html', '2019-05-30');
-
-Pages::add_page('About', 'about.html');

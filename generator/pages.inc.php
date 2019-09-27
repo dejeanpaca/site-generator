65ff54a02673bb9ed208575786cd15a0042d7fa1
Dejan Boras <de.jean.7777@protonmail.com>
2019-09-27 15:58:25 +0200

    Remove functionality which is no longer used

--- generator/pages.inc.php
+++ generator/pages.inc.php
@@ -9,27 +9,6 @@ class Pages
     /** @var Page[] */
     public static $list = [];
 
-    public static function add_post($source) {
-        $page = new Post();
-        $page->source = $source;
-
-        self::add($page);
-    }
-
-    public static function add_page($source) {
-        $page = new Page();
-        $page->source = $source;
-
-        self::add($page);
-    }
-
-    public static function add_index($source) {
-        $page = new IndexPage();
-        $page->source = $source;
-
-        self::add($page);
-    }
-
     public static function add($page) {
         array_push(self::$list, $page);
 

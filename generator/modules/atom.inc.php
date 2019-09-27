c7d264e51513b8a8a7895c41066fa4f74a780b93
Dejan Boras <de.jean.7777@protonmail.com>
2019-09-27 16:14:20 +0200

    Module for generating atom feeds is now functional

--- generator/modules/atom.inc.php
+++ generator/modules/atom.inc.php
@@ -3,6 +3,7 @@
 class AtomModule extends Module
 {
     public const TEMPLATES_PATH = 'templates' . DIRECTORY_SEPARATOR . 'atom' . DIRECTORY_SEPARATOR;
+    public const FEED_FILE_NAME = 'feed.atom';
 
     /** @var AtomModule */
     public static $module;
@@ -11,6 +12,11 @@ class AtomModule extends Module
     public $feed_template = null;
     /** @var Replacer */
     public $entry_template = null;
+
+    public $entries = [];
+
+    /** latest date found in any post (to base feed update time on) */
+    public $lastDate = null;
 
     public function __construct() {
         $this->name = 'Atom';
@@ -21,11 +27,70 @@ class AtomModule extends Module
     public function Load() {
         $this->feed_template = new Replacer();
         $this->feed_template->file_path = self::TEMPLATES_PATH . 'feed.atom';
+        $this->feed_template->Load();
+
         $this->entry_template = new Replacer();
         $this->entry_template->file_path = self::TEMPLATES_PATH . 'entry.atom';
+        $this->entry_template->Load();
     }
 
+    /** @param Page $post */
     public function OnPost($post) {
+        // only process pages which have a feed uuid marker
+        if(!$post->HasMarker('__FEED_UUID__')) {
+            return;
+        }
+
+        if($post->date) {
+            if($this->lastDate) {
+                if($post->date > $this->lastDate)
+                    $this->lastDate = $post->date;
+            } else
+                $this->lastDate = $post->date;
+        }
+
+        $entry = substr($this->entry_template->content, 0);
+
+        // set time updated if none
+        if(!$post->HasMarker('__TIME_UPDATED__')) {
+            $entry = str_replace('__TIME_UPDATED__', $post->getDate(), $entry);
+        }
+
+        // set empty summary if none
+        if(!$post->HasMarker('__SUMMARY__')) {
+            $entry = str_replace('__SUMMARY__', '', $entry);
+        }
+
+
+        $entry = $post->Inject($entry);
+
+        array_push($this->entries, $entry);
+    }
+
+    public function Done() {
+        $feed = substr($this->feed_template->content, 0);
+        $feed = Common::Inject($feed);
+
+        $entries = '';
+
+        foreach ($this->entries as $entry) {
+            $entries = $entries . $entry;
+        }
+
+        $feed = str_replace('__FEED_ENTRIES__', $entries, $feed);
+
+        $updated_time = '';
+        if($this->lastDate)
+            $updated_time = date("c", $this->lastDate);
+
+        $feed = str_replace('__FEED_TIME_UPDATED__', $updated_time, $feed);
+
+        $fn = Common::$target . self::FEED_FILE_NAME;
+
+        $ok = write_file($fn, $feed, false);
+
+        if(!$ok)
+            fail('Failed to write feed file: ' . $fn);
     }
 }
 

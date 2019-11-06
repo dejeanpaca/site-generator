0a52eebaffe89c7bb9bae266a639b81c675f3617
Dejan Boras <de.jean.7777@protonmail.com>
2019-11-06 23:09:12 +0100

    Add a php tidy module

---
+++ source/modules/tidy.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+class TidyModule extends Module
+{
+    /** @var TidyModule */
+    public static $module;
+    /** @var [] */
+    public static $configuration;
+    public static $encoding = 'utf8';
+
+    public function __construct() {
+        $this->name = 'tidy';
+
+        self::$configuration = [
+            'indent' => false
+        ];
+
+        parent::__construct();
+    }
+
+    public function Done() {
+        foreach(Pages::$list as $page) {
+            $fn = $page->getTargetFn();
+
+            writeln('Tidy: ' . $fn);
+
+            $tidy = new tidy($fn, self::$configuration, self::$encoding);
+
+            if($tidy->cleanRepair()) {
+                $output = tidy_get_output($tidy);
+                $page->generated = $output;
+                $page->Write(true);
+            } else
+                writeln('Failed to tidy: ' . $fn);
+        }
+    }
+}
+
+TidyModule::$module = new TidyModule();
